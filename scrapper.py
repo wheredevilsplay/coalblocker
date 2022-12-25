3c382c2e864ab86e2a193e96711009c7ebba34a5
nyima <whereangelsplay@protonmail.com>
2022-12-25 20:07:58 +0000

    thanks kitchen utinsel

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,9 @@ import re
 from bs4 import BeautifulSoup
 import base64
 
+#TODO
+# if last scrapped post doesnt exist default to 1
+
 # get last scrapped post
 f = open("lastScrapped.txt", "r")
 firstPost = f.read()
@@ -16,7 +19,7 @@ lastPost = negro["data-post-id"]
 # scrapper 
 for x in range(int(firstPost), int(lastPost)):
     try:
-        posts = "https://booru.soy/post/view/" + str(x+1)
+        posts = "https://booru.soy/post/view/" + str(x)
         request = requests.get(posts)
         soup = BeautifulSoup(request.text, features="lxml")
         negro = soup.find("img", class_="shm-main-image")
@@ -24,16 +27,12 @@ for x in range(int(firstPost), int(lastPost)):
         md5NotEncoded = re.findall(r'[0-9a-f]{32}', md5NotEncoded)
         md5NotEncoded = md5NotEncoded[0] 
         md5NotEncoded = bytes.fromhex(md5NotEncoded)
-        md5Encoded = base64.b64encode(md5NotEncoded)
-        md5Encoded = str(md5Encoded)
-        #why the fuck did it add b' and ' to it dumb fukcing gorrila 
-        md5Encoded = md5Encoded[:-1]
-        md5Encoded = md5Encoded[2:]
+        md5Encoded = base64.b64encode(md5NotEncoded).decode()
         md5Encoded = "/" + md5Encoded + "/"
-        print(str(x+1) + " " + md5Encoded)
+        print(str(x) + " " + md5Encoded)
         with open("md5.txt", "a") as file_object:
             file_object.write(md5Encoded + "\n")
         with open("lastScrapped.txt", "w") as file_object:
-            file_object.write(str(x+1))
+            file_object.write(str(x))
     except:
         pass
